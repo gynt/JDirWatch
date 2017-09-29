2d4297058c761ae37e06bdea86f86f6cafdf845c
gynt <gynt@users.noreply.github.com>
2017-09-29 17:01:13 +0200

    eventfactory

--- src/com/gynt/jdirwatch/events/windows/EventFactory.java
+++ src/com/gynt/jdirwatch/events/windows/EventFactory.java
@@ -9,5 +9,12 @@ public class EventFactory {
 	public boolean isMoveEvent(RawEvent create, RawEvent delete) {
 		return create.getRelativePath().getFileName().equals(delete.getRelativePath().getFileName()) && (!(create.getFullPath().equals(delete.getFullPath())));
 	}
+	
+	public boolean isDirDeleteEvent(RawEvent delete) {
+		return false;
+		//TODO : How do we know/knew it was a directory?
+	}
+	
+	
 
 }
